893842ba2a7285a03f10e451a5a45b0843d6214b
Burglak <131761951+Burglak@users.noreply.github.com>
2024-01-29 01:43:22 +0100

    Create README.md

---
+++ README.m
@@ -0,0 +1,12 @@
+# PokemonTeamManager
+
+Simple application to manage your pokemon team. Designed for project for college.
+
+## Installation
+
+-Install app in your desired location
+-Installer automatically places the database file in C:\baza_pokemon
+
+## Login panel
+
+Login panel is just an example of how it may look like if I implemented accounts for each user
